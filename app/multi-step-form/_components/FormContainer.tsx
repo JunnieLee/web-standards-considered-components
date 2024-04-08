6390809e4aa65664347a203bf9b785f121837c4d
JunnieLee <heunghari@naver.com>
2024-04-08 18:12:09 +0900

    feat: 필요한 이미지 에셋 추가 + FormContainer 컴포넌트 추가

---
+++ app/multi-step-form/_components/FormContainer.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import React, { useState } from "react";
+
+const _FormContainer = () => {
+  const [step, setStep] = useState<number>(1); // step 1~ step 4 까지 있음
+
+  return (
+    <>
+      <label>
+        Text input: <input name="myInput" defaultValue="Some initial value" />
+      </label>
+      <hr />
+      <label>
+        Checkbox:{" "}
+        <input type="checkbox" name="myCheckbox" defaultChecked={true} />
+      </label>
+      <hr />
+      <p>
+        Radio buttons:
+        <label>
+          <input type="radio" name="myRadio" value="option1" />
+          Option 1
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="myRadio"
+            value="option2"
+            defaultChecked={true}
+          />
+          Option 2
+        </label>
+        <label>
+          <input type="radio" name="myRadio" value="option3" />
+          Option 3
+        </label>
+      </p>
+    </>
+  );
+};
+
+export const FormContainer = React.memo(_FormContainer);
--- app/multi-step-form/page.tsx
+++ app/multi-step-form/page.tsx
@@ -1,42 +1,59 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { FormContainer } from "./_components/FormContainer";
 
-export default function MultiStepFormPage() {
-  return (
-    <main style={{ flex: 1, height: "100vh", padding: 40 }}>
-      <p style={{ fontSize: 40, paddingTop: 20, paddingBottom: 20 }}>
-        Multi-Step-Form Page
-      </p>
-      <label>
-        Text input: <input name="myInput" defaultValue="Some initial value" />
-      </label>
-      <hr />
-      <label>
-        Checkbox:{" "}
-        <input type="checkbox" name="myCheckbox" defaultChecked={true} />
-      </label>
-      <hr />
+export default function multiStepFormPage() {
+  const pageTitle = (
+    <p
+      style={{
+        fontSize: 40,
+        paddingTop: 20,
+        paddingBottom: 20,
+        fontWeight: "bold",
+      }}
+    >
+      Multi-Step-Form Page
+    </p>
+  );
+
+  const guideSection = (
+    <div
+      style={{
+        display: "flex",
+        width: 550,
+        flexDirection: "column",
+        padding: 30,
+        marginTop: 10,
+        marginBottom: 20,
+        backgroundColor: "white",
+        borderRadius: 8,
+      }}
+    >
+      <h2 style={{ paddingBottom: 20 }}>웹 표준을 위한 구현 고려사항</h2>
+      <p>1. 라벨을 클릭하면 input이 선택되야 합니다.</p>
+      <p>2. 모든 input에는 라벨이 있어야 합니다.</p>
+      <p>3. 키보드 만으로 폼을 모두 제출할 수 있어야 합니다.</p>
       <p>
-        Radio buttons:
-        <label>
-          <input type="radio" name="myRadio" value="option1" />
-          Option 1
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="myRadio"
-            value="option2"
-            defaultChecked={true}
-          />
-          Option 2
-        </label>
-        <label>
-          <input type="radio" name="myRadio" value="option3" />
-          Option 3
-        </label>
+        4. 폼에 한 글자를 입력할 때마다 폼 전체가 리렌더링되어서는 안 됩니다.
       </p>
+    </div>
+  );
+
+  return (
+    <main
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        height: "100vh",
+        padding: 40,
+        justifyContent: "flex-start",
+        alignItems: "center",
+      }}
+    >
+      <header>{pageTitle}</header>
+      <section>{guideSection}</section>
+      <FormContainer />
     </main>
   );
 }
