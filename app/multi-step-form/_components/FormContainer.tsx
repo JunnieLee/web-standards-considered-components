b8c20f7d04ae6b2848e567126b939c2269d431e3
JunnieLee <heunghari@naver.com>
2024-04-10 17:31:59 +0900

    feat: radio input으로 다중택일 인풋 형태 생성

--- app/multi-step-form/_components/FormContainer.tsx
+++ app/multi-step-form/_components/FormContainer.tsx
@@ -40,6 +40,7 @@ export const FormContainer = () => {
       ...Object.fromEntries(formData), // ex. {"username":"김태희","email":"rabolution@gmail.com","phoneNumber":"010-4827-1733"}
     }));
     if (step < 4) {
+      alert(JSON.stringify(formState)); // tmp
       router.push("/multi-step-form?step=" + (step + 1));
     } else {
       // TODO :
@@ -97,7 +98,30 @@ export const FormContainer = () => {
         <p id={id + "-description" + 2} className={styles.descriptionText}>
           연 단위, 달 단위의 플랜을 선택하실 수 있습니다.
         </p>
-        <button type="submit">Next Step</button>
+        <section
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            flex: 1,
+            marginTop: 20,
+          }}
+        >
+          <label className={styles.card}>
+            베이직
+            <input type="radio" name="plan" value="basic" defaultChecked />
+          </label>
+          <label className={styles.card}>
+            플러스
+            <input type="radio" name="plan" value="plus" />
+          </label>
+          <label className={styles.card}>
+            프로
+            <input type="radio" name="plan" value="pro" />
+          </label>
+        </section>
+        <button type="submit" className={styles.submitButton}>
+          다음 단계
+        </button>
       </form>
     </section>
   );
